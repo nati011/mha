970c0fb06bf663116e299a514641cb2f24ec6e9c
natnaelj <natnaeljemaneh001@gmail.com>
2026-01-24 14:04:30 +0300

    Prefer request host for resource blog fetch

--- app/(frontend)/resources/[slug]/page.tsx
+++ app/(frontend)/resources/[slug]/page.tsx
@@ -4,17 +4,18 @@ import { headers } from 'next/headers'
 import { notFound } from 'next/navigation'
 
 function getBaseUrl() {
-  const envBaseUrl = process.env.NEXT_PUBLIC_BASE_URL
-  if (envBaseUrl) {
-    return envBaseUrl.replace(/\/$/, '')
-  }
-
   const headerList = headers()
-  const host = headerList.get('x-forwarded-host') ?? headerList.get('host')
+  const hostHeader = headerList.get('x-forwarded-host') ?? headerList.get('host')
+  const host = hostHeader?.split(',')[0]?.trim()
   const protocol = headerList.get('x-forwarded-proto') ?? 'http'
 
   if (host) {
     return `${protocol}://${host}`
+  }
+
+  const envBaseUrl = process.env.NEXT_PUBLIC_BASE_URL
+  if (envBaseUrl) {
+    return envBaseUrl.replace(/\/$/, '')
   }
 
   return 'http://localhost:3000'
